59528a24ccb0ad2249b3f1bd2027f9669c9575cf
Krystian Kałużny <k.kaluzny141@gmail.com>
2019-12-06 13:50:23 +0100

    Add sql file

---
+++ database.sql
@@ -0,0 +1,34 @@
+CREATE TABLE user_groups(
+    id INT AUTO_INCREMENT,
+    name VARCHAR(255),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE users(
+    id INT AUTO_INCREMENT,
+    username VARCHAR(255),
+    email VARCHAR(255),
+    password VARCHAR(245),
+    user_group_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_group_id) REFERENCES user_groups(id)
+);
+
+CREATE TABLE exercises(
+    id INT AUTO_INCREMENT,
+    title VARCHAR(255),
+    description TEXT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE solutions(
+    id INT AUTO_INCREMENT,
+    created DATETIME,
+    updated DATETIME,
+    description TEXT,
+    exercise_id INT NOT NULL ,
+    user_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (exercise_id) REFERENCES exercises(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
