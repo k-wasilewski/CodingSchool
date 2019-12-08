964a2fca7f29868ac6a9cbeec95397504c488558
Krystian Kałużny <k.kaluzny141@gmail.com>
2019-12-08 16:08:14 +0100

    Move code from Main class to UserTestProcessor

--- src/org/school/test/Main.java
+++ src/org/school/test/Main.java
@@ -1,50 +1,10 @@
 package org.school.test;
 
-import org.school.dao.UserDao;
-import org.school.User;
-
 class Main {
 
     public static void main(String[] args) {
 
-        UserDao userDao = new UserDao();
-
-        User newUser = new User("test", "test@test.com", "test123");
-        User createdUser = userDao.create(newUser);
-
-        System.out.println("Create user: " + createdUser);
-
-
-        User readUser = userDao.read(100);
-        System.out.println("Read user: " + readUser);
-
-        System.out.println("--------------------");
-        User userBeforeUpdate = userDao.read(2);
-        System.out.println("User before update: " + userBeforeUpdate);
-
-        User userToUpdate = new User("abc", "def@test.com", "abc12345");
-        userToUpdate.setId(2);
-
-        userDao.update(userToUpdate);
-
-        User userAfterUpdate = userDao.read(2);
-        System.out.println("User after update: " + userAfterUpdate);
-
-
-        System.out.println("--------------------");
-        User userBeforeDelete = userDao.read(5);
-        System.out.println("User before delete: " + userBeforeDelete);
-
-        userDao.delete(5);
-
-        User userAfterDelete = userDao.read(5);
-        System.out.println("User after delete: " + userAfterDelete);
-
-        System.out.println("--------------------");
-        User[] allUsers = userDao.findAll();
-        System.out.println("All users:");
-        for (User user : allUsers) {
-            System.out.println(user);
-        }
+        Processor userTestProcessor = new UserTestProcessor();
+        userTestProcessor.run();
     }
 }
